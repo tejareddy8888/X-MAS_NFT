887cbfa8e768fee414e449c2d7bb17cddc09a27a
borrito <yh01dlx@gmail.com>
2022-10-22 15:34:42 +0200

    feat(wip): temporarily save the DappNew for testing

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Dapp } from './components/Dapp';
+// import { Dapp } from './components/Dapp';
+import { Dapp } from './components/DappNew';
 
 // We import bootstrap here, but you can remove if you want
 import 'bootstrap/dist/css/bootstrap.css';
