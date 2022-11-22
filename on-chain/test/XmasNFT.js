8e16c1c7e2382688f8cdd3026b494b3392a5ad4b
borrito <yh01dlx@gmail.com>
2022-11-22 23:03:43 +0100

    refactor: why not capital case stupit me

--- on-chain/test/XmasNFT.js
+++ on-chain/test/XmasNFT.js
@@ -19,7 +19,7 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 // `describe` receives the name of a section of your test suite, and a
 // callback. The callback must define the tests of that section. This callback
 // can't be an async function.
-describe("XmasNFT contract", function () {
+describe("XmasNFT Contract", function () {
   // We define a fixture to reuse the same setup in every test. We use
   // loadFixture to run this setup once, snapshot that state, and reset Hardhat
   // Network to that snapshot in every test.
